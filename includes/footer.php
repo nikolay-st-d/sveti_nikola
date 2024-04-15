75dc6fe1dc4a798b0881911c9ac54de77fcc7ce9
Nikolay St. Dimitrov <126271396+nikolay-st-d@users.noreply.github.com>
2024-04-15 10:42:41 +0300

    Some tweaks

--- includes/footer.php
+++ includes/footer.php
@@ -2,34 +2,17 @@
   <div class="footer">
     &copy;
     <?php echo DATE("Y"); ?> Къща за гости СВЕТИ НИКОЛА<br>
-    <span class="small_text">&copy; 2023 <a target="_blank" href="https://www.linoart.com"
-        title="Linoart - Graphic Design and Web Development Studio">Linoart - уеб-програмиране и дизайн</a></span>
+    <span class="small_text">&copy; 2023 <a target="_blank" href="https://www.linoart.com/index-bg.php"
+        title="Изработка на уникални уеб-сайтове, уеб-дизайн, програмиране">Linoart - уеб-програмиране и дизайн</a></span>
   </div>
 </div>
 
 <?php
-
-function getUserIP()
-{
-  $client = @$_SERVER['HTTP_CLIENT_IP'];
-  $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
-  $remote = $_SERVER['REMOTE_ADDR'];
-
-  if (filter_var($client, FILTER_VALIDATE_IP)) {
-    $ip = $client;
-  } elseif (filter_var($forward, FILTER_VALIDATE_IP)) {
-    $ip = $forward;
-  } else {
-    $ip = $remote;
-  }
-
-  return $ip;
-}
 // IP logger
-// $ip_addr = getUserIP();
-// $page_uri = "$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-// $data = $ip_addr . '|' . date("Y-m-d H:i:s") . '|' . $page_uri . '' . PHP_EOL;
-// $file = fopen('vis_log.txt', 'a+');
-// fwrite($file, $data);
-// fclose($file);
+$ip_addr = $_SERVER['REMOTE_ADDR'];
+$page_uri = "$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$data = $ip_addr . '|' . date("Y-m-d H:i:s") . '|' . $page_uri . '' . PHP_EOL;
+$file = fopen('vis_log.txt', 'a+');
+fwrite($file, $data);
+fclose($file);
 ?>
--- includes/head.php
+++ includes/head.php
@@ -1,21 +1,28 @@
 <!DOCTYPE html>
 <html lang="bg">
-
 <head>
-    <title>
-        <?php echo $title; ?>
-    </title>
+    <title><?php echo $title; ?></title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="description" content="<?php echo $description; ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="styles.css?ver=1.3" rel="stylesheet" type="text/css">
     <META NAME="ROBOTS" content="index, follow">
     <meta name="GOOGLEBOT" content="index, follow">
-    <link rel="shortcut icon" href="images/favicon.png">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet"
-        type="text/css">
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Shantell+Sans:wght@400;800&display=swap" rel="stylesheet">
+    <link rel="apple-touch-icon" sizes="57x57" href="apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+    <link rel="manifest" href="manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
 </head>
